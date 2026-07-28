0330213186ad5d8629a07cbf81347bbfc5e5f9d0
Jeevan <jeevan9901855474@gmail.com>
2026-07-28 19:39:42 +0530

    Time: 8 ms (52.45%), Space: 44.1 MB (47.99%) - LeetHub

---
+++ 0567-permutation-in-string/0567-permutation-in-string.java
@@ -0,0 +1,28 @@
+class Solution {
+    public boolean checkInclusion(String s1, String s2) {
+        if(s1.length() > s2.length()) return false;
+
+        int[] s1count = new int[26];
+        int[] s2count = new int[26];
+
+        for(int i =0; i< s1.length() ; i++){
+            s1count[s1.charAt(i) - 'a']++;
+            s2count[s2.charAt(i) - 'a']++;
+        }
+
+        for( int i = 0; i < s2.length() - s1.length();i++){
+            if(matches(s1count,s2count)) return true;
+
+            s2count[s2.charAt(i + s1.length()) - 'a']++;
+            s2count[s2.charAt(i) - 'a']--;
+        }
+        return matches(s1count,s2count);
+    }
+
+    public boolean matches(int[] s1count, int[] s2count){
+        for(int i =0;i< 26;i++){
+            if(s1count[i]!= s2count[i]) return false;
+        }
+        return true;
+    }
+}
